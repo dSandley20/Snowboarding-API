27cd0c5335539928ce64c5d777c050f4cdb32118
dSandley20 <danielsandley2@gmail.com>
2021-04-07 23:15:22 -0400

    initial setup

---
+++ src/main/java/com/dsandley/main/SnowboardApiApplication.java
@@ -0,0 +1,13 @@
+package com.dsandley.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowboardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowboardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/snowboard/SnowboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snowboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
