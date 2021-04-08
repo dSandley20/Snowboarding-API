12c8f02e70f62104d9b0d966c892026cd56cb04b
dSandley20 <danielsandley2@gmail.com>
2021-04-08 01:15:01 -0400

    comment out bean logger

--- src/main/java/com/dsandley/SnowboardApiApplication.java
+++ src/main/java/com/dsandley/SnowboardApiApplication.java
@@ -18,18 +18,18 @@ public class SnowboardApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SnowboardApiApplication.class, args);
 	}
-
-	@Bean
-	  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-	    return args -> {
-
-	      System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-	      String[] beanNames = ctx.getBeanDefinitionNames();
-	      Arrays.sort(beanNames);
-	      for (String beanName : beanNames) {
-	        System.out.println(beanName);
-	      }
-	    };
-	  }    
+//
+//	@Bean
+//	  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//	    return args -> {
+//
+//	      System.out.println("Let's inspect the beans provided by Spring Boot:");
+//
+//	      String[] beanNames = ctx.getBeanDefinitionNames();
+//	      Arrays.sort(beanNames);
+//	      for (String beanName : beanNames) {
+//	        System.out.println(beanName);
+//	      }
+//	    };
+//	  }    
 }
