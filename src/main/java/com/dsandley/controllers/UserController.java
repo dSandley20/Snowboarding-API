9eacd7ae067450e6097e252263f7df1909ead20a
dSandley20 <danielsandley2@gmail.com>
2021-08-20 12:53:30 -0400

    getting the API spun up again

--- src/main/java/com/dsandley/controllers/UserController.java
+++ src/main/java/com/dsandley/controllers/UserController.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dsandley.models.general.User;
@@ -22,9 +24,28 @@ public class UserController {
     private UserService service;
 
     /**
+     * Creates a new user to login with
+     * @return
+     */
+    @PostMapping("/user")
+    public String home(){
+        return "Hello World!";
+    }
+
+    /**
+     * Returns a user by ID
+     */
+    @GetMapping("/user/:id")
+    public User getUser(){
+        return null;
+    }
+
+
+    /**
      * returns back all the users in the DB.
      * @return List<User>
      */
+
     @GetMapping("/users")
     public List<User> findUsers() {
         return service.findAll();
