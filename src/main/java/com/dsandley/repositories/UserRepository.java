216a15abde5cb68a00562a41ca498308b8d36263
dSandley20 <danielsandley2@gmail.com>
2021-08-23 14:52:38 -0400

    added query to find user -> need to change to database way rather than through user list

--- src/main/java/com/dsandley/repositories/UserRepository.java
+++ src/main/java/com/dsandley/repositories/UserRepository.java
@@ -3,7 +3,11 @@
  */
 package com.dsandley.repositories;
 
+
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.dsandley.models.general.User;
@@ -12,5 +16,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
-    Optional<User> findByUserName(String userName);
+    @Query("SELECT u FROM User u WHERE u.userName LIKE :username")
+    public User getUserByUsername(@Param("username") String username);
 }
