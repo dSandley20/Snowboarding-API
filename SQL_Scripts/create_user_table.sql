8f34bf09c58541c4fbdc99fd9326aa476449d75c
dSandley20 <danielsandley2@gmail.com>
2021-08-23 13:40:49 -0400

    added test user as well as updated user table

--- SQL_Scripts/create_user_table.sql
+++ SQL_Scripts/create_user_table.sql
@@ -2,5 +2,8 @@ CREATE TABLE snowboard_db.users (
 	id serial NOT NULL,
 	first_name varchar(255) NOT NULL,
 	last_name varchar(255) NOT NULL,
-	email varchar(255) NOT NULL
+	email varchar(255) NOT NULL,
+	phone varchar(12) NOT NULL,
+	user_name varchar(50) NOT NULL,
+	password varchar(300) NOT NULL
 );
---
+++ SQL_Scripts/test_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users
+(first_name, last_name, email, phone, user_name, "password")
+VALUES('Daniel', 'Sandley', 'danielsandley2@gmail.com', '+18435325598', 'user', 'pass');
