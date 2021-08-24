0e458252cdead59744c3158284de211c0c691176
dSandley20 <danielsandley2@gmail.com>
2021-08-23 20:39:44 -0400

    using ByCrypt as password encoder

--- src/main/java/com/dsandley/authentication/SecurityConfigurer.java
+++ src/main/java/com/dsandley/authentication/SecurityConfigurer.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -24,7 +25,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
      */
     @Bean
     public static PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     /**
