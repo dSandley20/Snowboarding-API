eb8651003c2e7222c4619bf847fa681c9a99f169
dSandley20 <danielsandley2@gmail.com>
2021-08-23 21:09:03 -0400

    userDTO to pass into registration endpoint

---
+++ src/main/java/com/dsandley/dto/general/UserDTO.java
@@ -0,0 +1,38 @@
+package com.dsandley.dto.general;
+
+public class UserDTO {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String userName;
+    private String password;
+
+    public UserDTO(String firstName, String lastName, String email, String userName, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
