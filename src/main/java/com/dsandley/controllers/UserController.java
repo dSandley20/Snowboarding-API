2b5c68d82152c84747d697e9de73877a83414853
dSandley20 <danielsandley2@gmail.com>
2021-08-23 23:27:41 -0400

    finish endpoint logic to register new user and return JWT

--- src/main/java/com/dsandley/controllers/UserController.java
+++ src/main/java/com/dsandley/controllers/UserController.java
@@ -5,7 +5,16 @@ package com.dsandley.controllers;
 
 import java.util.List;
 
+import com.dsandley.dto.general.UserDTO;
+import com.dsandley.models.authentication.AuthenticationResponse;
+import com.dsandley.services.AuthUserDetailsService;
+import com.dsandley.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import com.dsandley.models.general.User;
@@ -19,16 +28,36 @@ public class UserController {
      */
     @Autowired
     private UserService service;
+    /**
+     * Allows us to actually authenticate our user/data.
+     */
+    @Autowired
+    private AuthenticationManager authManager;
+
+    /**
+     * Allows us to create the token itself.
+     */
+    @Autowired
+    private AuthUserDetailsService userDetailsService;
+
+    /**
+     * Allows us to use the util functions to create our token.
+     */
+    @Autowired
+    private JwtUtil jwtTokenUtil;
 
     /**
      * Creates a new user to login with
      * @return
      */
     @PostMapping("/user")
-    private User createUser(@RequestBody User user){
-        service.createUser(user);
+    private ResponseEntity<?> createUser(@RequestBody UserDTO userDTO) throws  Exception{
+        service.createUser(userDTO);
         //return userObj back to user w/ JWT.
-        return  user;
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(userDTO.getUserName());
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
 
     /**
